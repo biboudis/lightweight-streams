48bf06e31cd778c92acf95702ad854f6570464ac
Aggelos Biboudis <biboudis@gmail.com>
2014-09-18 05:51:20 +0300

    Added test.

---
+++ src/test/java/Test_SimplePipelines.java
@@ -0,0 +1,17 @@
+import benchmarks.SimplePipelines;
+import org.junit.Test;
+
+public class Test_SimplePipelines {
+
+    static SimplePipelines __benchClass = new SimplePipelines();
+
+    @Test
+    public void Test_cart(){
+        assert (__benchClass.cart_Boxed_Long_LStreams().equals(__benchClass.cart_Boxed_Long_Java8Streams()));
+    }
+
+    @Test
+    public void Test_map_filter_fold_Boxed_Long(){
+        assert (__benchClass.map_filter_fold_Boxed_Long_LStreams().equals(__benchClass.map_filter_fold_Boxed_Long_Java8Streams()));
+    }
+}
